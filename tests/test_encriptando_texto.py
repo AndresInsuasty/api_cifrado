61c9ad9acdba046357250d7d72fee3c6246ba8f6
andresinsuasty <andresinsuastyd10@gmail.com>
2023-08-14 22:26:31 -0500

    fix: deuda tecnica pylint

--- tests/test_encriptando_texto.py
+++ tests/test_encriptando_texto.py
@@ -2,8 +2,8 @@
 Aqui se prueba la funcion de encriptado simetrico
 """
 import pytest
-from app.funciones.encriptado import encriptando_texto
 from cryptography.fernet import Fernet
+from app.funciones.encriptado import encriptando_texto
 
 @pytest.mark.parametrize(
         "mensaje,llave",
@@ -23,7 +23,13 @@ from cryptography.fernet import Fernet
         b"UyJ4iWnYDPOcZ3sCmwNZtKbIudZRYl5n7owVTiQ4r3o="
         )
         ])
-def test_encriptando_texto(mensaje,llave):
+def test_encriptando_texto(mensaje:str,llave:str):
+    """ funcion para probar la encriptacion de textos
+
+    Args:
+        mensaje (str): Mensaje a ser encriptado
+        llave (str): llave de encriptacion simetrica
+    """
     entorno_cifrado = Fernet(llave)
     salida = encriptando_texto(mensaje,llave)
     mensaje_desencriptado = entorno_cifrado.decrypt(salida)
