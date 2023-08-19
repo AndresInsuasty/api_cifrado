29563c2ab83231f23e5551ad19f63026a81457d5
andresinsuasty <andresinsuastyd10@gmail.com>
2023-08-18 23:11:10 -0500

    desarrollo: pruebas url cifrado cesar

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
-"""Este es el modulo principal"""
+"""Este es el modulo principal donde habrá la creacion y disposición de una API"""
+from fastapi import FastAPI
 from funciones.cifrado_cesar import cifrado_cesar
 
-MENSAJE = "Este es mi mensaje"
-CLAVE = 2
+app = FastAPI()
 
-resultado = cifrado_cesar(MENSAJE,CLAVE)
-print(resultado)
+@app.get("/")
+def home():
+    """
+    Funcion principal de API
+    """
+    return {"Hola":"Mundo123"}
+
+@app.get("/cifrado-cesar")
+def url_cifrado_cesar(texto,clave):
+    """
+    URL para hacer encriptado CESAR
+    """
+    salida = cifrado_cesar(texto,int(clave))
+    return {"mensaje_cifrado":salida}
